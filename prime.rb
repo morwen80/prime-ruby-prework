5ebba8127b7f9a867c0d1b4a95d35de6a0333b53
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-24 18:06:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,6 @@
-# Add  code here!
 require 'prime'
+
 def prime?(int) 
-  
-  
-  # if int % 2 == 0 
-  #   return false
-  #   elsif int <=0 
-  #   return false
-  # else
-  #   return true
-  # end
   return int.prime?
 
 end
