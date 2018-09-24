015ac236045ef4ccf7fab1359ce42258b6c87373
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-24 17:11:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,9 @@
 # Add  code here!
 def prime?(int) 
   
-  x = 2 
-  while int % 2 == 0 
-  
-  
-  if int % 2 == 0 || int <= 0
+  if int % 2 == 0 
+    return false
+    elsif int <=0 
     return false
   else
     return true
